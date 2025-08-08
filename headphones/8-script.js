13f16562e1f889ad6cd1859f3c4e8449260ee248
philemon <philemonmaosa@gmail.com>
2025-08-09 02:47:09 +0300

    I have edited the files

---
+++ headphones/8-script.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById('hamburger');
+const nav = document.querySelector('nav');
+
+
+
+  hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+}
